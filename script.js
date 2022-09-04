bc86bdcd46278c4afa7d66277abb681311161f66
Tarun Yadav <tarunvyadav@gamil>
2022-09-05 02:04:05 +0530

    second change

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ function opentab(tabname){
         document.getElementById(tabname).classList.add('active-tab')
  
 }
+
+
+var sidemenu = document.getElementById("sidemenu")
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
